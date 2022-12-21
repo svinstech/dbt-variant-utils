ddebc513693da358f1c18635c818daefbde73944
Jenna Allen <jenna.allen@vouch.us>
2022-12-21 08:53:37 -0800

    add string to number test

--- integration_tests/models/dim_as_primitive.sql
+++ integration_tests/models/dim_as_primitive.sql
@@ -2,5 +2,6 @@ select
     {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'same_type') }} as cast_same_type,
     {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'with_null') }} as cast_with_null,
     {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'different_types') }} as cast_different_types,
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'decimal_number') }} as cast_decimal_number
+    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'decimal_number') }} as cast_decimal_number,
+    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'string_number', try_casting=True) }} as cast_string_number
 from {{ ref('stg_as_primitive') }}
