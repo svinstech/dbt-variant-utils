5ff0705ab9ecd8e78d191be183d3976d1b408caa
Jenna Allen <jenna.allen@vouch.us>
2023-03-26 17:27:00 -0700

    need to rethink tests here

--- integration_tests/models/dim_as_primitive.sql
+++ integration_tests/models/dim_as_primitive.sql
@@ -1,7 +1,7 @@
-select
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'same_type') }} as cast_same_type,
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'with_null') }} as cast_with_null,
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'different_types') }} as cast_different_types,
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'decimal_number') }} as cast_decimal_number,
-    {{ dbt_variant_utils.as_primitive(ref('stg_as_primitive'), 'string_number', try_casting=True) }} as cast_string_number
-from {{ ref('stg_as_primitive') }}
+select 
+    {{ dbt_variant_utils.as_primitive('integer', "get(extract, 'same_type')", '2') }} cast_same_type,
+    {{ dbt_variant_utils.as_primitive('varchar', "get(extract, 'with_null')", '2') }} cast_with_null,
+    {{ dbt_variant_utils.as_primitive('boolean', "get(extract, 'different_types')", '2') }} cast_different_types,
+    {{ dbt_variant_utils.as_primitive('decimal', "get(extract, 'decimal_number')", '9') }} cast_decimal_number,
+    {{ dbt_variant_utils.as_primitive('decimal', "get(extract, 'string_number')", '2', try_casting=True) }} cast_string_number
+from {{ ref('stg_as_primitive')}}
